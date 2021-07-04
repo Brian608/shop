e7bf40086b3c5e62df4fa048f63610c6f6a405a3
feather <brian608@163.com>
2021-07-04 14:54:37 +0800

    :construction: nacos注册

--- order-service/src/main/java/org/feather/OrderApplication.java
+++ order-service/src/main/java/org/feather/OrderApplication.java
@@ -2,6 +2,8 @@ package org.feather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,10 +13,12 @@ import org.springframework.web.client.RestTemplate;
  * @author: 杜雪松(feather)
  * @create: 2021-07-03 20:38
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate create(){
         return  new RestTemplate();
     }
