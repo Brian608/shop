82e2a8b467e38358f3bce80c50a857177d72b900
feather <brian608@163.com>
2021-07-04 15:06:58 +0800

    :wrench:nacos  注册

--- stock-service/src/main/java/org/feather/StockApplication.java
+++ stock-service/src/main/java/org/feather/StockApplication.java
@@ -2,6 +2,7 @@ package org.feather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @program: EShopParent
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author: 杜雪松(feather)
  * @create: 2021-07-04 09:45
  **/
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StockApplication {
     public static void main(String[] args) {
