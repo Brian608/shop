83085033fabda3a4e7f8469f3dcae03d3eb74570
feather <brian608@163.com>
2021-07-06 23:15:58 +0800

    :rocket:添加秒杀配置

---
+++ skill-service/src/main/java/org/feather/SkillApplication.java
@@ -0,0 +1,34 @@
+package org.feather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @program: EShopParent
+ * @description:
+ * @author: 杜雪松(feather)
+ * @create: 2021-07-06 23:05
+ **/
+@SpringBootApplication
+public class SkillApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkillApplication.class);
+    }
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        RedisTemplate<Object,Object> template=new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        //采用普通的key 字符串
+        template.setStringSerializer(new StringRedisSerializer());
+        return  template;
+    }
+    @Bean
+    public RestTemplate restTemplate(){
+        return  new RestTemplate();
+    }
+}
